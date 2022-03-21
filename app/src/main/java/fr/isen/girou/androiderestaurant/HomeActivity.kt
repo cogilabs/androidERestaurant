b24259e86f104c08003e9ed8a4d7715f259e4486
0Davgi0 <davgi.wifeo@gmail.com>
2022-03-21 15:02:47 +0100

    Ajout dans l'index

---
+++ app/src/main/java/fr/isen/girou/androiderestaurant/HomeActivity.kt
@@ -0,0 +1,39 @@
+package fr.isen.girou.androiderestaurant
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import fr.isen.girou.androiderestaurant.databinding.ActivityHomeBinding
+
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+
+    private fun toastMsg(msg: String?) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    fun displayEntreesMsg(view: View) {
+        toastMsg("Entrées")
+    }
+
+    fun displayPlatsMsg(view: View) {
+        toastMsg("Plats")
+    }
+
+    fun displayDessertsMsg(view: View) {
+        toastMsg("Desserts")
+    }
+
+}
