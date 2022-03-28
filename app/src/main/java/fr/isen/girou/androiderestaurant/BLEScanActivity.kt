64db1e968b5e6fb89f30be1429353f92445f6846
0Davgi0 <davgi.wifeo@gmail.com>
2022-03-28 14:06:19 +0200

    Nettoyage du code (2)

--- app/src/main/java/fr/isen/girou/androiderestaurant/BLEScanActivity.kt
+++ app/src/main/java/fr/isen/girou/androiderestaurant/BLEScanActivity.kt
@@ -5,12 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import fr.isen.girou.androiderestaurant.databinding.ActivityBlescanBinding
-import android.widget.ProgressBar
 
 class BLEScanActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBlescanBinding
-    private var progressBar: ProgressBar? = null
     private var scanning = false
 
     @SuppressLint("SetTextI18n")
@@ -20,22 +18,20 @@ class BLEScanActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        progressBar = findViewById<ProgressBar>(R.id.scanBLEProgressBar) as ProgressBar
-
         binding.scanBLEText.text = "Lancer le scan"
         binding.scanBLEBtn.setImageResource(R.drawable.ic_baseline_play_arrow_50)
-        progressBar!!.visibility = View.INVISIBLE
-        progressBar!!.isIndeterminate = false
+        binding.scanBLEProgressBar.visibility = View.INVISIBLE
+        binding.scanBLEProgressBar.isIndeterminate = true
 
             binding.scanBLEBtn.setOnClickListener {
                 if (scanning) {
                     binding.scanBLEText.text = "Lancer le scan"
                     binding.scanBLEBtn.setImageResource(R.drawable.ic_baseline_play_arrow_50)
-                    progressBar!!.visibility = View.INVISIBLE
+                    binding.scanBLEProgressBar.visibility = View.INVISIBLE
                 } else {
                     binding.scanBLEText.text = "Scan en cours..."
                     binding.scanBLEBtn.setImageResource(R.drawable.ic_baseline_pause_50)
-                    progressBar!!.visibility = View.VISIBLE
+                    binding.scanBLEProgressBar.visibility = View.VISIBLE
                 }
                 scanning = !scanning
             }
